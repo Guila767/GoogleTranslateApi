01bdf0bd74ae7315f4a451c1ac093630186ebccc
guilherme masena ladario <gui.ladario@hotmail.com>
2021-02-04 18:58:54 -0300

    Changes: - Renamed the API wrapper class name -  The 'Language' class is now independent of the 'GoogleTranslator' class - Some minor changes to the code
    Added:
    - A test/example project to the solution
    
    > GoogleTranslator (The API wrapper):
    - Asynchronous methods
    
    > Language
    - A parse method
    - A method to get the full language name

---
+++ GoogleTranslateApiExample/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GoogleTranslateApiExample
+{
+    class Program
+    {
+        static void Main(string[] args) => new Example(args).StartAsync().GetAwaiter().GetResult();
+    }
+
+    class Example
+    {
+        private readonly GoogleTranslateApi.Language source = GoogleTranslateApi.Language.English;
+        private readonly GoogleTranslateApi.Language target = GoogleTranslateApi.Language.Portuguese;
+        private readonly GoogleTranslateApi.GoogleTranslator translate;
+
+        public Example() : this(Array.Empty<string>()) { }
+        
+        public Example(string[] args) 
+        {
+            if (args.Length == 2)
+            {
+                source = GoogleTranslateApi.Language.Parse(args[0]);
+                target = GoogleTranslateApi.Language.Parse(args[1]);
+            }
+
+            this.translate = new GoogleTranslateApi.GoogleTranslator(
+                source,
+                target
+            );
+        }
+
+        public async Task StartAsync()
+        {
+            string text;
+            Console.WriteLine("Type '!exit' to close the test sample...\n");
+            while((text = Console.ReadLine()) != "!exit")
+            {
+                Console.WriteLine($"> Translation from {source.GetLanguageFullName()} to {target.GetLanguageFullName()}:");
+                Console.Out.WriteLine(await this.translate.GetTextAsync(text));
+            }
+        }
+    }
+    //static class TextWriterEx
+    //{
+    //    public static async Task OnTextReadAsync(this TextReader textReader, Action<string> action, CancellationToken cancellationToken)
+    //    {
+    //        while (!cancellationToken.IsCancellationRequested)
+    //        {
+    //            var r = await textReader.ReadLineAsync();
+    //            action.Invoke(r);
+    //        }
+    //    }
+
+    //    public static async Task OnTextReadAsync(this TextReader textReader, Action<string> action) => await OnTextReadAsync(textReader, action, CancellationToken.None);
+    //}
+}
